f04954c94d8f8a93838b143459139bfef3ae54f2
Ope <o_orekoya@yahoo.com>
2023-01-22 13:43:54 +0000

    Added functionalty ofr js and html

--- starter/assets/js/script.js
+++ starter/assets/js/script.js
@@ -1,6 +1,10 @@
 // Show the current Day at the top
 //  Use moment api and format todays date
 //  Add to currentDay element
+let currentDay = $('#currentDay')
+let todaysDate = moment().format(' dddd, MMM Do');
+currentDay.text(todaysDate)
+let inputs = []
 
 // Colour code each block based on current time
 //  Create variables to target each time block
@@ -16,6 +20,21 @@
 //       else
 //          this.css('background-color, futureColor)    
 //})
+let thisHour = moment().format('HH')
+let timeBlocks = $('.time-block')
+$.each(timeBlocks, function(){
+    var hour = parseInt($(this).attr('data-hour'))
+    if(hour < thisHour){
+        $(this).css({'background-color': '#d3d3d3', 'color': 'white'})
+    }
+    else if(hour == thisHour){
+        $(this).css({'background-color': '#ff6961', 'color': 'white'})
+    }
+    else{
+        $(this).css({'background-color': '#77dd77', 'color': 'white'})
+    }
+})
+
 
 // Save input to local storage
 //  create variable (an array) called inputs that will store all of the inputdata
@@ -41,6 +60,23 @@
 //      }]   
 //  
 
+$('.saveBtn').on('click', function(event){
+    event.preventDefault()
+    let parent = $(this).parent()
+
+    let input = parent.children('textarea').val()
+
+    let time = parent.attr('data-hour')
+
+    inputs.push({'time': time, 'input': input })
+    inputs = JSON.stringify(inputs)
+
+    localStorage.setItem('inputs', inputs)
+    inputs = JSON.parse(inputs)
+
+    var message = $("<br><br><br><p>"+ input + " has been saved into the diary... </p>").fadeOut(3000)
+    currentDay.append(message)
+})
 
 // load input from local storage when page loads.refresh if theres any data in local storage
 //      var localStorageInput = get data from local storage 
@@ -58,4 +94,16 @@
 
 // Extra, user feedback when they save the timeblock
 // Extra, add hover on the save button
+$(document).ready(function() {
+    var localStorageInput = localStorage.getItem('inputs')
+    localStorageInput = JSON.parse(localStorageInput)
+if(localStorageInput){
+    
+    localStorageInput.forEach( function(input){
+        console.log(input.time)
+        $('.time-block[data-hour="' + input.time + '"] textarea').val(input.input)
+    })
+}
+
+})
 
